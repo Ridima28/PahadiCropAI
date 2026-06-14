5ee8b8dcc0045463005ce77436ada11d19e5b5a2
Ridima28 <ridimafinal@gmail.com>
2026-06-14 15:59:09 +0530

    feat: navbar is set up with navlinks, and routing is added.

---
+++ Frontend/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Navbar from '../components/Landing/Navbar';
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-background">
+      <Navbar />
+    </div>
+  );
+}
