f6c5cf55c8ddd9c3b35ec91c8898e9456eb34177
Ridima28 <ridimafinal@gmail.com>
2026-06-25 19:19:07 +0530

    update: added email and username in mongo schema

--- Backend/server.js
+++ Backend/server.js
@@ -2,8 +2,11 @@ import express from 'express'
 import 'dotenv/config'
 import cors from 'cors'
 import userRouter from './routes/userRoutes'
+import { connectDB } from './configs/db.js'
 
 const app = express()
+
+await connectDB()
 
 //middleware
 
