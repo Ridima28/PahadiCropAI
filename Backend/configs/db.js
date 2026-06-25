8fad4009013dfaf7440fd1aa55235bf33b32238f
Ridima28 <ridimafinal@gmail.com>
2026-06-25 19:09:17 +0530

    update: mongodb connected

--- Backend/configs/db.js
+++ Backend/configs/db.js
@@ -1,9 +1,11 @@
 import mongoose from 'mongoose';
 
-const connectDB = async () =>{
+export const connectDB = async () =>{
     try{
+        mongoose.connection.on('connected', ()=> console.log("Database Conneced"))
         await mongoose.connect(`${process.env.MONGODB_URI}/PahadiCrop`);
     } catch (err){
+        console.log(err.message)
         
     }
 };
